df2435934e188ca7928c0167be2252c1b7e08d57
Patricia <patriciamedeiros41@gmail.com>
2022-08-24 19:04:54 -0300

    1-variacao-e-valor

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,12 @@
+
+let nome 
+let email 
+let idade 
+let profissao 
+let paisdossonhos 
+
+nome = "patricia"
+email = "patriciamedeiros41@gmail.com"
+idade = "26"
+profissao = "estudante"
+paisdossonhos = "paris"
