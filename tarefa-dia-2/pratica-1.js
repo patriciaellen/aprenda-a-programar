0e1f93c8e5915f0b473facbf84328fda86057d67
Patricia <patriciamedeiros41@gmail.com>
2022-08-24 21:14:15 -0300

    aula 2 desafio opcional

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,18 +1,24 @@
+// Variação
 
 let nome 
 let email 
 let idade 
 let profissao 
-let paisdossonhos 
+let paisDosSonhos 
 
-nome = "patricia"
+// Valor
+nome = "Patricia"
 email = "patriciamedeiros41@gmail.com"
-idade = "26"
+idade = 26
 profissao = "estudante"
-paisdossonhos = "paris"
+paisDosSonhos = "Paris"
 
-console.log(nome)
-console.log(email)
-console.log(idade)
-console.log(profissao)
-console.log(paisdossonhos)
+
+// Template strings
+let texto = `Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. 
+Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${paisDosSonhos}. 
+Confirma pra gente alguns dados? A sua idade é ${idade}, 
+você é ${profissao} e o seu email é ${email}? 
+Ficamos no aguardo. Muito obrigado e boa viagem! :D`
+console.log(texto)
+
