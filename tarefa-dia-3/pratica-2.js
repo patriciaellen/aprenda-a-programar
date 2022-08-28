5e49655df97749498fb43be5fc96031b9a115d6e
patricia <patriciamedeiros41@gmail.com>
2022-08-28 17:28:23 -0300

    Resposta pratica 2

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,8 @@
+let numeroInicial = 1
+let numeroFinal = 10 
+let numeroTabuada = 9
+
+while(numeroInicial <= numeroFinal){
+    console.log(`${numeroTabuada} x ${numeroInicial} = ${numeroTabuada*numeroInicial}`) 
+    numeroInicial = numeroInicial + 1 
+}
